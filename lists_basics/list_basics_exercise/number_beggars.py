e580949974a48a880cb25bd9527cbf3a05385d92
Петър Попов <popovstz@gmail.com>
2023-09-30 14:24:17 +0300

    solved problem 4

---
+++ lists_basics/list_basics_exercise/number_beggars.py
@@ -0,0 +1,15 @@
+string_of_integers = [int(s) for s in input().split(", ")]
+number_of_beggars = int(input())
+acuminated_sums = []
+start_from = 0
+
+for _ in range(number_of_beggars):
+    acuminated_sum = 0
+    for i in range(start_from, len(string_of_integers), number_of_beggars):
+        acuminated_sum += string_of_integers[i]
+    start_from += 1
+    acuminated_sums.append(acuminated_sum)
+
+print(acuminated_sums)
+
+
