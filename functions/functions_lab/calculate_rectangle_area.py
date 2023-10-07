0c22014660485ef4b0de36edab1ca64108d1af39
Петър Попов <popovstz@gmail.com>
2023-10-07 13:44:54 +0300

    solved problem 6

---
+++ functions/functions_lab/calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+def cal_rectangle_area(hight: int, width: int) -> int:
+    return hight * width
+
+
+input_height = int(input())
+input_width = int(input())
+
+print(cal_rectangle_area(input_height, input_width))
