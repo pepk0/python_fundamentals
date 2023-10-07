49153d7ca200091e1ca0773275f5bba895d3cbf0
Петър Попов <popovstz@gmail.com>
2023-10-07 14:35:10 +0300

    updated problem 4 solution

--- functions/functions_exercise/odd_even_sum.py
+++ functions/functions_exercise/odd_even_sum.py
@@ -1,9 +1,8 @@
-def even_odd_sum(num: int) -> tuple:
+def even_odd_sum(num: str) -> tuple:
     even_result = 0
     odd_result = 0
-    num_as_str = str(num)
-    for i in range(len(num_as_str)):
-        num_as_int = int(num_as_str[i])
+    for i in range(len(num)):
+        num_as_int = int(num[i])
         if num_as_int % 2 == 0:
             even_result += num_as_int
         else:
@@ -11,6 +10,6 @@ def even_odd_sum(num: int) -> tuple:
     return even_result, odd_result
 
 
-number = int(input())
+number = input()
 even_sum, odd_sum = even_odd_sum(number)
 print(f"Odd sum = {odd_sum}, Even sum = {even_sum}")
