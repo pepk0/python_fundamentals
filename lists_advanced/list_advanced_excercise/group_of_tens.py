582e9fa0afb2ace885cd5fe506a26fcd505872da
Петър Попов <popovstz@gmail.com>
2023-10-14 14:38:38 +0300

    solved problem 7

--- lists_advanced/list_advanced_excercise/group_of_tens.py
+++ lists_advanced/list_advanced_excercise/group_of_tens.py
@@ -3,4 +3,4 @@ max_element = max(list_of_elements)
 
 for group in range(10, max_element + 10, 10):
     print(f"Group of {group}'s: "
-          f"{[i for i in list_of_elements if group - 10 <i <= group]}")
+          f"{[i for i in list_of_elements if group - 10 < i <= group]}")
