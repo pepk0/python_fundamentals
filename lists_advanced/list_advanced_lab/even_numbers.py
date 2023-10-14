bf8bb67c2162d8c1612ceb6669bfbe7c0e3a4c0b
Петър Попов <popovstz@gmail.com>
2023-10-14 11:59:37 +0300

    solved problem 6

---
+++ lists_advanced/list_advanced_lab/even_numbers.py
@@ -0,0 +1,6 @@
+def condition(x): return x % 2 == 0
+
+
+number_list = [int(i) for i in input().split(", ")]
+even_list = [i for i in range(len(number_list)) if condition(number_list[i])]
+print(even_list)
