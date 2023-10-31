aae885aaf97bc86e5d2c570b76ab83aa2162da2f
Петър Попов <popovstz@gmail.com>
2023-10-31 21:13:53 +0200

    refactor

--- dictionaries/dictionaries_lab/bakery.py
+++ dictionaries/dictionaries_lab/bakery.py
@@ -1,5 +1,5 @@
-def make_dict(str_lit: list) -> dict:
-    return {str_lit[i]: int(str_lit[i + 1]) for i in range(0, len(str_lit), 2)}
+def make_dict(words: list) -> dict:
+    return {words[i]: int(words[i + 1]) for i in range(0, len(words), 2)}
 
 
 print(make_dict(input().split()))
