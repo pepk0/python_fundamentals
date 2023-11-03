5e0d5e9c1874e258bebc699aad0b4d9ba4cff83e
Петър Попов <popovstz@gmail.com>
2023-11-03 22:09:05 +0200

    solved problem 4

---
+++ dictionaries/dictionaries_exsercise/phonebook.py
@@ -0,0 +1,26 @@
+def fill_up_phonebook(name: str, number: str, phonebook: dict) -> None:
+    if name not in phonebook:
+        phonebook[name] = ""
+    phonebook[name] = number
+
+
+def find_user(user_name: str, phonebook: dict) -> str:
+    result = f"Contact {user_name} does not exist."
+    if user_name in phonebook:
+        result = f"{user_name} -> {phonebook[user_name]}"
+    return result
+
+
+contact_phonebook = {}
+number_searches = 0
+while 1:
+    user_information = input().split("-")
+    if len(user_information) <= 1:
+        number_searches = int(user_information[0])
+        break
+    user_name, user_phone_number = user_information
+    fill_up_phonebook(user_name, user_phone_number, contact_phonebook)
+
+for _ in range(number_searches):
+    user_query = input()
+    print(find_user(user_query, contact_phonebook))
