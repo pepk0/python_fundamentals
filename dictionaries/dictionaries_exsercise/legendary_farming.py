c1222484fbf83294f77f9833ebb7c7648bf27195
Петър Попов <popovstz@gmail.com>
2023-11-04 13:19:15 +0200

    solved problem 5

---
+++ dictionaries/dictionaries_exsercise/legendary_farming.py
@@ -0,0 +1,31 @@
+def gather_materials(items: list, resources_dict: dict, legendaries: dict):
+    for index in range(0, len(items), 2):
+        material = items[index + 1].lower()
+        if material not in resources_dict:
+            resources_dict[material] = 0
+        resources_dict[material] += int(items[index])
+        for material in legendaries:
+            if resources_dict[material] >= 250:
+                resources_dict[material] -= 250
+                return f"{legendary_items[material]} obtained!"
+
+
+resources = {
+    "shards": 0,
+    "fragments": 0,
+    "motes": 0
+}
+legendary_items = {
+    "motes": "Dragonwrath",
+    "fragments": "Valanyr",
+    "shards": "Shadowmourne"
+}
+
+while 1:
+    materials = input().split()
+    to_print = gather_materials(materials, resources, legendary_items)
+    if to_print:
+        print(to_print)
+        break
+
+print(*[f"{k}: {v}" for k, v in resources.items()], sep="\n")
