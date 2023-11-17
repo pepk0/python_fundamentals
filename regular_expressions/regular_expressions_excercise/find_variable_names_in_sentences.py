9c5ecbbd2019edcc4c161f4b18df7055a6a5116b
pepk0 <popovstz@gmail.com>
2023-11-17 19:15:03 +0200

    refactored problem 2

--- regular_expressions/regular_expressions_excercise/find_variable_names_in_sentences.py
+++ regular_expressions/regular_expressions_excercise/find_variable_names_in_sentences.py
@@ -2,9 +2,9 @@ import re
 
 
 def extract_variable_names(text: str) -> list:
-    pattern = r"\b(_){1}([A-Za-z0-9]+)\b"
+    pattern = r"\b(?:_)([A-Za-z0-9]+)\b"
     matches = re.findall(pattern, text)
-    return [var_name[1] for var_name in matches]
+    return [var_name for var_name in matches]
 
 
 input_text = input()
