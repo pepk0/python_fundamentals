8c40d632102dc6c0caf86e86dffe22ef54841392
pepk0 <popovstz@gmail.com>
2023-11-18 15:04:24 +0200

    refactored problem 4

--- regular_expressions/regular_expressions_more/nether_realms.py
+++ regular_expressions/regular_expressions_more/nether_realms.py
@@ -10,13 +10,9 @@ def get_demon_health(text: str) -> int:
 
 
 def calculate_damage(text: str) -> float:
-    base_damage = 0
     multiply_divide = re.findall(r"[\/\*]", text)
     number_matches = re.findall(r"(\-?[0-9]+[\.0-9]*)", text)
-    for number in number_matches:
-        if "-" in number:
-            number = float(number.replace("-", "")) * -1
-        base_damage += float(number)
+    base_damage = sum([float(i) for i in number_matches])
     for operation in multiply_divide:
         if operation == "*":
             base_damage *= 2
@@ -26,12 +22,7 @@ def calculate_damage(text: str) -> float:
 
 
 demons = re.sub(" ", "", input()).split(",")
-demon_stats = {}
-for demon in demons:
-    demon_health = get_demon_health(demon)
-    demon_attack = calculate_damage(demon)
-    demon_stats[demon] = (demon_health, demon_attack)
-
-for demon_name, (health, damage) in sorted(demon_stats.items(),
-                                           key=lambda x: x[0]):
-    print(f"{demon_name} - {health} health, {damage:.2f} damage")
+for demon in sorted(demons):
+    health = get_demon_health(demon)  
+    damage = calculate_damage(demon)  
+    print(f"{demon} - {health} health, {damage:.2f} damage")
