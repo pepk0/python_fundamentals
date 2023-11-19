86b58852f97cefbd7392d933e36cdc2d1caa765d
pepk0 <popovstz@gmail.com>
2023-11-19 13:14:57 +0200

    refactored problem 5

--- regular_expressions/regular_expressions_more/html_parser.py
+++ regular_expressions/regular_expressions_more/html_parser.py
@@ -3,7 +3,7 @@ import re
 
 def extract_title(text: str) -> str:
     pattern = r"<title>(.*)<\/title>"
-    title = re.search(pattern, text).group(1) # type: ignore
+    title = re.search(pattern, text).group(1)  # type: ignore
     title = re.sub(r"\\n|\\t", "", title)
     title = re.sub(r"<[^>]*>", "", title)
     return title
@@ -11,7 +11,7 @@ def extract_title(text: str) -> str:
 
 def extract_content(text: str) -> str:
     match_pattern = r"<body>(.*)</body>"
-    content = re.search(match_pattern, text).group(1) #type: ignore
+    content = re.search(match_pattern, text).group(1)  # type: ignore
     content = re.sub(r"\\n|\\t", "", content)
     content = re.sub(r"<[^>]*>", "", content)
     return content
